8f02fb89227820ff12b03292afcc8de67ca5494e
Ghabriel <ghabriel.nunes@gmail.com>
2016-07-25 17:08:08 -0300

    More CFG tests

--- tests/CFG_test.cpp
+++ tests/CFG_test.cpp
@@ -11,19 +11,17 @@ protected:
 };
 
 TEST_F(TestCFG, SymbolLists) {
-    ASSERT_NO_THROW(cfg.add("S", {"a", "<A>", "b"}, {"<&>"}));
-    ASSERT_NO_THROW(cfg.add("A", {"a", "<A>"}, {"b", "<A>"}, {"<&>"}));
-    set expectedTerminals = {"a", "b"};
-    set expectedNonTerminals = {"S", "A"};
-    EXPECT_EQ(cfg.getTerminals(), expectedTerminals);
-    EXPECT_EQ(cfg.getNonTerminals(), expectedNonTerminals);
+    ASSERT_NO_THROW(cfg.add("S", "a<A>b", "<&>"));
+    ASSERT_NO_THROW(cfg.add("A", "a<A>", "b<A>", "<&>"));
+    EXPECT_EQ(set({"a", "b"}), cfg.getTerminals());
+    EXPECT_EQ(set({"S", "A"}), cfg.getNonTerminals());
 }
 
 TEST_F(TestCFG, Consistency) {
     EXPECT_TRUE(cfg.isConsistent());
-    cfg.add("S", {"a", "<A>", "b"}, {"<&>"});
+    cfg.add("S", "a<A>b", "<&>");
     EXPECT_FALSE(cfg.isConsistent());
-    cfg.add("A", {"a", "<A>"}, {"b", "<A>"}, {"<&>"});
+    cfg.add("A", "a<A>", "b<A>", "<&>");
     EXPECT_TRUE(cfg.isConsistent());
 }
 
@@ -31,14 +29,50 @@ TEST_F(TestCFG, Range) {
     cfg.add("<S> ::= <S>a | <A><B> | c");
     cfg.add("<A> ::= d<A> | <A>e | <&>");
     cfg.add("<B> ::= b");
-    set expRangeS = {"S", "A", "B"};
-    set expRangeA = {"A"};
-    set expRangeB;
 
-    cfg.updateRangeTable();
-    EXPECT_EQ(expRangeS, cfg.range("S"));
-    EXPECT_EQ(expRangeA, cfg.range("A"));
-    EXPECT_EQ(expRangeB, cfg.range("B"));
+    EXPECT_EQ(set({"S", "A", "B"}), cfg.range("<S>"));
+    EXPECT_EQ(set({"A"}), cfg.range("<A>"));
+    EXPECT_EQ(set(), cfg.range("<B>"));
+}
+
+TEST_F(TestCFG, First) {
+    cfg.add("<E> ::= <T><E1>");
+    cfg.add("<E1> ::= +<T><E1> | <&>");
+    cfg.add("<T> ::= <F><T1>");
+    cfg.add("<T1> ::= *<F><T1> | <&>");
+    cfg.add("<F> ::= (<E>) | id");
+
+    EXPECT_EQ(set({"(", "id"}), cfg.first("<E>"));
+    EXPECT_EQ(set({"+"}), cfg.first("<E1>"));
+    EXPECT_EQ(set({"(", "id"}), cfg.first("<T>"));
+    EXPECT_EQ(set({"*"}), cfg.first("<T1>"));
+    EXPECT_EQ(set({"(", "id"}), cfg.first("<F>"));
+
+    EXPECT_FALSE(cfg.nullable("<E>"));
+    EXPECT_TRUE(cfg.nullable("<E1>"));
+    EXPECT_FALSE(cfg.nullable("<T>"));
+    EXPECT_TRUE(cfg.nullable("<T1>"));
+    EXPECT_FALSE(cfg.nullable("<F>"));
+    EXPECT_EQ(set({"+", "(", "id"}), cfg.first("<E1><E>"));
+
+    cfg.clear();
+    cfg.add("<S> ::= <S>a | <&>");
+    EXPECT_EQ(set({"a"}), cfg.first("<S>"));
+    EXPECT_TRUE(cfg.nullable("<S>"));
+
+    cfg.clear();
+    cfg.add("<S> ::= <A><B><C><S>e | <&>");
+    cfg.add("<A> ::= a<A> | <&>");
+    cfg.add("<B> ::= b<B> | <&>");
+    cfg.add("<C> ::= c<C> | <&>");
+    EXPECT_EQ(set({"a", "b", "c", "e"}). cfg.first("<S>"));
+    EXPECT_EQ(set({"a"}). cfg.first("<A>"));
+    EXPECT_EQ(set({"a"}). cfg.first("<B>"));
+    EXPECT_EQ(set({"a"}). cfg.first("<C>"));
+    EXPECT_TRUE(cfg.nullable("<S>"));
+    EXPECT_TRUE(cfg.nullable("<A>"));
+    EXPECT_TRUE(cfg.nullable("<B>"));
+    EXPECT_TRUE(cfg.nullable("<C>"));
 }
 
 int main(int argc, char** argv) {
