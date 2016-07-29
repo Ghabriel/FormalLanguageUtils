ae9f209cc9ea685bd2180e3f9c1b996bdcab092d
Ghabriel <ghabriel.nunes@gmail.com>
2016-07-29 13:48:22 -0300

    Removed useless files and continued the implementation of SimplifiedBNF

--- include/CFGRepresentation.hpp
+++ include/CFGRepresentation.hpp
@@ -3,6 +3,9 @@
 #ifndef CFGREPRESENTATION_HPP
 #define CFGREPRESENTATION_HPP
 
+#include <string>
+#include <functional>
+
 struct ProductionParts {
 	std::string name;
 	std::vector<std::string> products;
@@ -12,7 +15,8 @@ class CFGRepresentation {
 public:
 	virtual bool isTerminal(const std::string&) const = 0;
 	virtual bool isNonTerminal(const std::string&) const = 0;
-	virtual ProductionParts decompose(const std::string&) const = 0;
+	virtual std::vector<ProductionParts> decompose(const std::string&) const = 0;
+	virtual std::vector<std::string> toSymbolSequence(const std::string&) const = 0;
 	virtual std::string toReadableForm() const = 0;
 };
 
--- include/SimplifiedBNF.hpp
+++ include/SimplifiedBNF.hpp
@@ -3,25 +3,57 @@
 #ifndef SIMPLIFIED_BNF_HPP
 #define SIMPLIFIED_BNF_HPP
 
+#include <regex>
+
 class SimplifiedBNF : public CFGRepresentation {
-	bool isTerminal(const std::string&) const override {
-		
+public:
+	bool isTerminal(const std::string& symbol) const override {
+	    return symbol[0] != '<';
+	}
+
+	bool isNonTerminal(const std::string& symbol) const override {
+		return symbol.front() == '<' && symbol.back() == '>';
+	}
+
+	std::vector<ProductionParts> decompose(const std::string& group) const override {
+	    const static std::regex valid("(<[A-Za-z0-9_']+>) ?::= ?(.*)");
+	    std::smatch matches;
+	    std::regex_match(group, matches, valid);
+	    assert(matches.size() > 0);
+	    std::string buffer;
+	    std::string productions = matches[2];
+	    std::vector<ProductionParts> result;
+	    for (char c : productions) {
+	        if (c == '|') {
+	            add(result, matches[1], buffer);
+	            buffer.clear();
+	        } else {
+	            buffer += c;
+	        }
+	    }
+	    add(result, matches[1], buffer);
+	    return result;
+	}
+
+	std::string toReadableForm(const std::string& name,
+		const std::vector<std::string>& products) const override {
+
+	    std::string result = name + " ::= ";
+	    for (auto& symbol : products) {
+	        result += symbol;
+	    }
+	    return result;
+	}
+
+private:
+	void add(std::vector<ProductionParts>& result,
+		const std::string& name, const std::string& buffer) const {
+
+		ProductionParts production;
+		production.name = name;
+		production.products = toSymbolSequence(buffer);
+		result.push_back(std::move(production));
 	}
 };
-
-
-
-// struct ProductionParts {
-// 	std::string name;
-// 	std::vector<std::string> products;
-// };
-
-// class CFGRepresentation {
-// public:
-// 	virtual bool isTerminal(const std::string&) const = 0;
-// 	virtual bool isNonTerminal(const std::string&) const = 0;
-// 	virtual ProductionParts decompose(const std::string&) const = 0;
-// 	virtual std::string toReadableForm() const = 0;
-// };
 
 #endif
